1f0b8c130b1a51894ebd5952e699361df3898598
FRYoussef <f.r.youssef@hotmail.com>
2020-04-15 18:34:52 +0200

    added plot scripts

---
+++ holdings_plot.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+import plotly.graph_objects as go
+
+if __name__ == '__main__':
+    filename: str = os.path.join('datawarehouse', 'census.csv')
+    df: pd.DataFrame = pd.read_csv(filename, header=0)
+
+    # calculate total holdings
+    df['holdings'] = df['ETH'] + df['GEN'] + df['otherTokens']
+    df = df.sort_values(by=['holdings'])
+
+    # holdings stacked bar
+    fig = go.Figure(data=[
+        go.Bar(name='ETH (USD)', x=df['name'], y=df['ETH']),
+        go.Bar(name='GEN (USD)', x=df['name'], y=df['GEN']),
+        go.Bar(name='Other criptos (USD)', x=df['name'], y=df['otherTokens'])
+    ])
+
+    # Change the bar mode
+    fig.update_layout(barmode='stack')
+    fig.show()
