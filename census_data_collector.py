3d797e45cb9ff5270fbf3bf0ef66d00cc9506320
FRYoussef <f.r.youssef@hotmail.com>
2020-04-15 13:25:29 +0200

    Added votes and stakes

--- census_data_collector.py
+++ census_data_collector.py
@@ -17,6 +17,12 @@ DAO_QUERY: str = '{{daos(where: {{register: "registered"}}, first: {0}, skip: {1
 PROPOSAL_QUERY: str = '{{proposals(where: {{dao: "{0}"}}, first: {1}, skip: {2}\
 ){{id}}}}'
 
+VOTES_QUERY: str = '{{proposalVotes(where: {{dao: "{0}"}}, first: {1}, skip: {2}\
+){{id}}}}'
+
+STAKES_QUERY: str = '{{proposalStakes(where: {{dao: "{0}"}}, first: {1}, skip: {2}\
+){{id}}}}'
+
 
 def request(query: str) -> Dict:
     """
@@ -103,10 +109,64 @@ def get_proposals() -> pd.DataFrame:
     return df
 
 
+def get_votes() -> pd.DataFrame:
+    """
+    Gets a dataframe with DAOs ids and them number of votes
+    """
+    df: pd.DataFrame = pd.DataFrame(columns=['id', 'n_votes'])
+    print("Requesting votes ...")
+    start: datetime = datetime.now()
+
+    for d_id in daos_ids:
+        votes: List[Dict] = n_requests(query=VOTES_QUERY, 
+            result_key='proposalVotes', dao_id=d_id)
+
+        dff: pd.DataFrame = pd.DataFrame(votes)
+        dff = dff.rename(columns={"id": "vId"})
+        dff['id'] = d_id
+        dff = dff.groupby('id').size().reset_index(name='n_votes')
+        if len(votes) == 0:
+            dff = {'id': d_id, 'n_votes': 0}
+
+        df = df.append(dff, ignore_index=True)
+
+    print(f'Votes requested in {(datetime.now() - start).total_seconds():.2}s')
+    return df
+
+
+def get_stakes() -> pd.DataFrame:
+    """
+    Gets a dataframe with DAOs ids and them number of stakes
+    """
+    df: pd.DataFrame = pd.DataFrame(columns=['id', 'n_stakes'])
+    print("Requesting stakes ...")
+    start: datetime = datetime.now()
+
+    for d_id in daos_ids:
+        stakes: List[Dict] = n_requests(query=STAKES_QUERY, 
+            result_key='proposalStakes', dao_id=d_id)
+
+        dff: pd.DataFrame = pd.DataFrame(stakes)
+        dff = dff.rename(columns={"id": "sId"})
+        dff['id'] = d_id
+        dff = dff.groupby('id').size().reset_index(name='n_stakes')
+        if len(stakes) == 0:
+            dff = {'id': d_id, 'n_stakes': 0}
+
+        df = df.append(dff, ignore_index=True)
+
+    print(f'Stakes requested in {(datetime.now() - start).total_seconds():.2}s')
+    return df
+
+
 if __name__ == '__main__':
     df1: pd.DataFrame = get_daos()
     df2: pd.DataFrame = get_proposals()
+    df3: pd.DataFrame = get_votes()
+    df4: pd.DataFrame = get_stakes()
 
     df = df1
     df['n_proposals'] = df2['n_proposals'].tolist()
+    df['n_votes'] = df3['n_votes'].tolist()
+    df['n_stakes'] = df4['n_stakes'].tolist()
     print(df)
