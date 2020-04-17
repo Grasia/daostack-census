8375aeeefea6f266e6bd050a0ab5501956c1a17e
FRYoussef <f.r.youssef@hotmail.com>
2020-04-17 09:56:31 +0200

    refactored requests

--- collectors/census_collector.py
+++ collectors/census_collector.py
@@ -1,15 +1,11 @@
 import os
-import json
 import pandas as pd
-from graphqlclient import GraphQLClient
 from typing import Dict, List
 from datetime import datetime
+from requester import n_requests
 
-
-ELEMS_PER_CHUNK: int = 1000
-DAOSTACK_URL: str = 'https://api.thegraph.com/subgraphs/name/daostack/master'
-client: GraphQLClient = GraphQLClient(DAOSTACK_URL)
 daos_ids: List[str] = list()
+
 
 # queries
 DAO_QUERY: str = '{{daos(where: {{register: "registered"}}, first: {0}, skip: {1}\
@@ -23,46 +19,6 @@ VOTES_QUERY: str = '{{proposalVotes(where: {{dao: "{0}"}}, first: {1}, skip: {2}
 
 STAKES_QUERY: str = '{{proposalStakes(where: {{dao: "{0}"}}, first: {1}, skip: {2}\
 ){{id}}}}'
-
-
-def request(query: str) -> Dict:
-    """
-    Requests data from endpoint.
-    """
-    result = client.execute(query)
-    result = json.loads(result)
-    return result['data'] if 'data' in result else dict()
-
-
-def n_requests(query: str, result_key: str, dao_id: str = '') -> List[Dict]:
-    """
-    Requests all chunks from endpoint.
-
-    Parameters:
-        * query: json to request
-        * result_key 
-        * dao_id
-    """
-    elements: List[Dict] = list()
-    chunk: int = 0
-    result = Dict
-    condition: bool = True
-
-    while condition:
-        if dao_id:
-            query_filled: str = query.format(dao_id, ELEMS_PER_CHUNK, len(elements))
-        else:
-            query_filled: str = query.format(ELEMS_PER_CHUNK, len(elements))
-
-        result = request(query=query_filled)
-        result = result[result_key]
-
-        elements.extend(result)
-
-        condition = len(result) == ELEMS_PER_CHUNK
-        chunk += 1
-
-    return elements
 
 
 def get_daos() -> pd.DataFrame:
