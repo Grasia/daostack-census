93a87ae0dc259cc1be3f03ab73774b1f381da737
FRYoussef <f.r.youssef@hotmail.com>
2020-04-17 15:24:53 +0200

    Added new distribution plots

--- ploters/distribution_plot.py
+++ ploters/distribution_plot.py
@@ -1,15 +1,20 @@
 import os
-import plotly.figure_factory as ff
+import plotly.express as px
 import pandas as pd
 
 
 if __name__ == '__main__':
-
     filename: str = os.path.join('datawarehouse', 'census.csv')
     df: pd.DataFrame = pd.read_csv(filename, header=0)
-    users: list = [df['n_users'].tolist()]
 
-    fig = ff.create_distplot(users, ['Users per DAO'], bin_size=[10], histnorm='probability')
-    fig.update_layout(legend={'orientation': 'h', 'x': 0, 'y': 1.15})
+    fig = px.histogram(df, 'n_users', nbins=50, labels={'n_users':'Users'})
+    fig.show()
 
+    fig = px.histogram(df, 'n_proposals', nbins=50, labels={'n_proposals':'Proposals'})
+    fig.show()
+
+    fig = px.histogram(df, 'n_votes', nbins=100, labels={'n_votes':'Votes'})
+    fig.show()
+
+    fig = px.histogram(df, 'n_stakes', nbins=20, labels={'n_stakes':'Stakes'})
     fig.show()
