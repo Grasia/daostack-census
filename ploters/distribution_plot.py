8712a9303288fc3d46391c418463b4abf9c6adf9
FRYoussef <f.r.youssef@hotmail.com>
2020-04-17 14:02:26 +0200

    density distribution issue fixed

--- ploters/distribution_plot.py
+++ ploters/distribution_plot.py
@@ -9,10 +9,7 @@ if __name__ == '__main__':
     df: pd.DataFrame = pd.read_csv(filename, header=0)
     users: list = [df['n_users'].tolist()]
 
-    fig = ff.create_distplot(users, ['Users per DAO'], bin_size=[5])
-    fig.update_layout(
-        xaxis_type="log",
-        yaxis_type="log",
-        legend={'orientation': 'h', 'x': 0, 'y': 1.15})
+    fig = ff.create_distplot(users, ['Users per DAO'], bin_size=[10], histnorm='probability')
+    fig.update_layout(legend={'orientation': 'h', 'x': 0, 'y': 1.15})
 
     fig.show()
