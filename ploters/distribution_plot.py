eb869f5c28770fbe1b1146d7907b8056bd34c68a
FRYoussef <f.r.youssef@hotmail.com>
2020-04-23 16:04:56 +0200

    Notation issue

--- ploters/distribution_plot.py
+++ ploters/distribution_plot.py
@@ -39,19 +39,9 @@ if __name__ == '__main__':
     filename: str = os.path.join('datawarehouse', 'census.csv')
     df: pd.DataFrame = pd.read_csv(filename, header=0)
 
-    # # users
-    # fig = go.Figure(data=[go.Histogram(
-    #     x=df['n_users'].tolist(), 
-    #     xbins={'size': 10.0},
-    #     marker_color=BLUE,
-    #     )])
-
-    # update_layout(fig=fig)
-    # fig.show()
-
-    # proposals
+    # users
     fig = go.Figure(data=[go.Histogram(
-        x=df['n_proposals'].tolist(), 
+        x=df['nUsers'].tolist(), 
         xbins={'size': 10.0},
         marker_color=BLUE,
         )])
@@ -59,22 +49,32 @@ if __name__ == '__main__':
     update_layout(fig=fig)
     fig.show()
 
-    # # votes
-    # fig = go.Figure(data=[go.Histogram(
-    #     x=df['n_votes'].tolist(), 
-    #     xbins={'size': 10.0},
-    #     marker_color=BLUE,
-    #     )])
+    # proposals
+    fig = go.Figure(data=[go.Histogram(
+        x=df['nProposals'].tolist(), 
+        xbins={'size': 10.0},
+        marker_color=BLUE,
+        )])
 
-    # update_layout(fig=fig)
-    # fig.show()
+    update_layout(fig=fig)
+    fig.show()
 
-    # # stakes
-    # fig = go.Figure(data=[go.Histogram(
-    #     x=df['n_stakes'].tolist(), 
-    #     xbins={'size': 10.0},
-    #     marker_color=BLUE,
-    #     )])
+    # votes
+    fig = go.Figure(data=[go.Histogram(
+        x=df['nVotes'].tolist(), 
+        xbins={'size': 10.0},
+        marker_color=BLUE,
+        )])
 
-    # update_layout(fig=fig)
-    # fig.show()
+    update_layout(fig=fig)
+    fig.show()
+
+    # stakes
+    fig = go.Figure(data=[go.Histogram(
+        x=df['nStakes'].tolist(), 
+        xbins={'size': 10.0},
+        marker_color=BLUE,
+        )])
+
+    update_layout(fig=fig)
+    fig.show()
