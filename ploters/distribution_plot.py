645a247d6e03236a1505339115dd377d48fd6ea3
FRYoussef <f.r.youssef@hotmail.com>
2020-05-28 15:28:43 +0200

    distribution plot improvements

--- ploters/distribution_plot.py
+++ ploters/distribution_plot.py
@@ -30,6 +30,8 @@ def update_layout(fig: go.Figure) -> None:
             'showline': True, 
             'linewidth': 2, 
             'linecolor': 'black',
+            'tick0': 0,
+            'dtick': 1,
         },
         plot_bgcolor="white",
     )
@@ -42,39 +44,67 @@ if __name__ == '__main__':
     # users
     fig = go.Figure(data=[go.Histogram(
         x=df['nUsers'].tolist(), 
-        xbins={'size': 10.0},
+        xbins={'size': 10.0, 'start': 1.0},
         marker_color=BLUE,
         )])
 
+    fig.add_trace(go.Scatter(
+        x=[0, 0], 
+        y=[0, len(df[df['nUsers'] == 0])],
+        line=dict(color='firebrick', width=4)
+        ))
+
     update_layout(fig=fig)
+    fig.update_layout(xaxis_title_text='Number of users', yaxis_title_text='Number of DAOs')
     fig.show()
 
     # proposals
     fig = go.Figure(data=[go.Histogram(
         x=df['nProposals'].tolist(), 
-        xbins={'size': 10.0},
+        xbins={'size': 10.0, 'start': 1.0},
         marker_color=BLUE,
         )])
 
+    fig.add_trace(go.Scatter(
+        x=[0, 0], 
+        y=[0, len(df[df['nProposals'] == 0])],
+        line=dict(color='firebrick', width=4)
+        ))
+
     update_layout(fig=fig)
+    fig.update_layout(xaxis_title_text='Number of proposals', yaxis_title_text='Number of DAOs')
     fig.show()
 
     # votes
     fig = go.Figure(data=[go.Histogram(
         x=df['nVotes'].tolist(), 
-        xbins={'size': 10.0},
+        xbins={'size': 10.0, 'start': 1.0},
         marker_color=BLUE,
         )])
 
+    fig.add_trace(go.Scatter(
+        x=[0, 0], 
+        y=[0, len(df[df['nVotes'] == 0])],
+        line=dict(color='firebrick', width=4)
+        ))
+
     update_layout(fig=fig)
+    fig.update_layout(xaxis_title_text='Number of votes', yaxis_title_text='Number of DAOs')
     fig.show()
 
     # stakes
     fig = go.Figure(data=[go.Histogram(
         x=df['nStakes'].tolist(), 
-        xbins={'size': 10.0},
+        xbins={'size': 10.0, 'start': 1.0},
         marker_color=BLUE,
         )])
 
+    fig.add_trace(go.Scatter(
+        x=[0, 0], 
+        y=[0, len(df[df['nStakes'] == 0])],
+        line=dict(color='firebrick', width=4)
+        ))
+
     update_layout(fig=fig)
+    fig.update_layout(xaxis_title_text='Number of stakes', yaxis_title_text='Number of DAOs')
     fig.show()
